e858d559e09f84a353d6a7a8c698cbfb093e1624
AmeyaLele <46368424+AmeyaLele@users.noreply.github.com>
2019-03-17 23:09:22 +0530

    Update MyApp.java
    Removed unused imports

--- src/main/java/com/ameya/starter/MyApp.java
+++ src/main/java/com/ameya/starter/MyApp.java
@@ -1,17 +1,12 @@
 package com.ameya.starter;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.ui.ModelMap;
-
-import com.ameya.model.MessageRepository;
 
 @SpringBootApplication
 @EntityScan(basePackages= {"com.ameya.model"})
